3ff17d62d49f6d8e2bf2413c653f87a5ee30610c
Gabriel <dallariva.gabriel@gmail.com>
2015-06-30 20:40:21 -0300

    Protractor

--- test/e2e/spec.js
+++ test/e2e/spec.js
@@ -1,7 +1,11 @@
 describe('Protractor Demo App', function() {
-  it('should have a title', function() {
-    browser.get('http://juliemr.github.io/protractor-demo/');
 
-    expect(browser.getTitle()).toEqual('Super Calculator');
-  });
+  it('Deve retornar 4.200,00 quando se passar o valor de 50', function() {
+        browser.driver.get('http://localhost:3000/tijolosComuns10cm');
+        browser.driver.findElement(by.id('numero1')).sendKeys(50);
+
+        browser.driver.findElement(by.id('botao')).click();
+
+        expect(browser.driver.findElement(by.id('res')).getText()).toEqual('O resultado é 4.200,00.');
+    });
 });
