ab69db4b7e119cbfc07a7b57af3c397c50b206f5
Gabriel <dallariva.gabriel@gmail.com>
2015-06-30 21:48:30 -0300

    d

--- test/e2e/spec.js
+++ test/e2e/spec.js
@@ -1,7 +1,7 @@
 describe('Protractor Demo App', function() {
 
   it('Deve retornar 4.200,00 quando se passar o valor de 50', function() {
-        browser.driver.get('http://localhost:3000/tijolosComuns10cm');
+        browser.driver.get('https://trabalho-pi-node.herokuapp.com/tijolosComuns10cm');
         browser.driver.findElement(by.id('numero1')).sendKeys(50);
 
         browser.driver.findElement(by.id('botao')).click();
